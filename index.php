ede76ac45965788dbcedb4becfde9a55086aa9ae
k018c1347 <k018c1347@it-neec.jp>
2020-11-17 12:19:59 +0900

    Test

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <body>
-    <h1>自己紹介</h1>;
-      <?php echo "Hello World! php" ?>
+    <h1>test</h1>;
+      <?php echo "Test php" ?>
       <ul>
-        <li>自己紹介</li>
-        <li>佐野。ハートビーツ社に今年7月入社。インフラエンジニア。現在は開発部門と人材育成部門のシニアマネージャー。Kpopと韓国好き。</li>
+        <li>Test1</li>
+        <li>Test</li>
         <li></li>
         <li></li>
       </ul>
       <ul>
-        <li>自己紹介</li>
-        <li>長谷川隼太。現在日本工学院専門学校3年生。エンジニア志望。最近ピアノを練習している。</li>
+        <li>Test2</li>
+        <li>???</li>
         <li></li>
         <li></li>
       </ul>
